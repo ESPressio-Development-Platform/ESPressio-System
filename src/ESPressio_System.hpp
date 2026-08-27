c3d2668ed5b88873aefe405db86f32cdcc093535
Simon J Stuart <lakraven@gmail.com>
2026-08-27 19:28:38 +0200

    refactor(system): expose collision-free platform clock header (#4, #29)

--- src/ESPressio_System.hpp
+++ src/ESPressio_System.hpp
@@ -5,7 +5,7 @@
 #include "ESPressio_Execution.hpp"
 #include "ESPressio_Synchronization.hpp"
 #include "ESPressio_Queue.hpp"
-#include "ESPressio_SystemClock.hpp"
+#include "ESPressio_SystemPlatformClock.hpp"
 #include "ESPressio_GPIO.hpp"
 #include "ESPressio_Entropy.hpp"
 #include "ESPressio_ByteStream.hpp"
