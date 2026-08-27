1af42f854077c256fc0a279cf07978f4a68c7c36
Simon J Stuart <lakraven@gmail.com>
2026-08-27 17:58:09 +0200

    feat(system): expose bounded queue abstraction (#4)

--- src/ESPressio_System.hpp
+++ src/ESPressio_System.hpp
@@ -4,5 +4,6 @@
 #include "ESPressio_Memory.hpp"
 #include "ESPressio_Execution.hpp"
 #include "ESPressio_Synchronization.hpp"
+#include "ESPressio_Queue.hpp"
 #include "ESPressio_Clock.hpp"
 #include "ESPressio_GPIO.hpp"
