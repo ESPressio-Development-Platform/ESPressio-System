6d51d976a28a52fab0edefdef00c53c8cba74e73
Simon J Stuart <lakraven@gmail.com>
2026-08-27 17:52:33 +0200

    feat(system): expose platform abstraction surface from umbrella header (#4)

--- src/ESPressio_System.hpp
+++ src/ESPressio_System.hpp
@@ -1,2 +1,8 @@
 #pragma once
+
+#include "ESPressio_Platform.hpp"
 #include "ESPressio_Memory.hpp"
+#include "ESPressio_Execution.hpp"
+#include "ESPressio_Synchronization.hpp"
+#include "ESPressio_Clock.hpp"
+#include "ESPressio_GPIO.hpp"
